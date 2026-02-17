1f7ec16c7c34d0ecf2f76e7c940df0c5c4c36e38
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2026-02-17 10:06:05 -0300

    feat: add possible to include multiples origins

--- src/env/env.ts
+++ src/env/env.ts
@@ -3,7 +3,10 @@ import { z } from 'zod';
 export const envSchema = z.object({
   NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
   PORT: z.coerce.number().default(3333),
-  CORS_ORIGIN: z.url(),
+  CORS_ORIGIN: z
+    .string()
+    .transform((val) => val.split(',').map((url) => url.trim()))
+    .pipe(z.array(z.url())),
 });
 
 export type Env = z.infer<typeof envSchema>;
