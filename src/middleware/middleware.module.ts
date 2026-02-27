69a1a63ca43daeb3104f4996ab7e273d6722f491
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2026-02-27 10:58:42 -0300

    feat: create logging middleware

--- src/middleware/middleware.module.ts
+++ src/middleware/middleware.module.ts
@@ -1,4 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ThrottlerModule } from '@nestjs/throttler';
+import { LoggingMiddleware } from './logging/logging.middleware';
 
-@Module({})
+@Module({
+  imports: [
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60_000, // 1 minute
+        limit: 100, // 100 requests
+      },
+    ]),
+  ],
+  providers: [LoggingMiddleware],
+})
 export class MiddlewareModule {}
