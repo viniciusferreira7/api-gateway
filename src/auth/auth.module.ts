9f20105985ee40785396926103508a9bf03becdf
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2026-03-24 09:58:04 -0300

    feat: setup auth module with JWT and Passport

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,24 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { EnvModule } from '@/env/env.module';
+import { EnvService } from '@/env/env.service';
+import { GrpcModule } from '@/grpc/grpc.module';
+import { AuthService } from './auth.service';
 
-@Module({})
+@Module({
+  imports: [
+    PassportModule,
+    GrpcModule,
+    JwtModule.registerAsync({
+      imports: [EnvModule],
+      useFactory: (envService: EnvService) => ({
+        secret: envService.get('JWT_SECRET'),
+        signOptions: { expiresIn: '24h' },
+      }),
+      inject: [EnvService],
+    }),
+  ],
+  providers: [AuthService],
+})
 export class AuthModule {}
