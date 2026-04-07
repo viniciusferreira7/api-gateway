47e2fd3e8bbb16d5224b0f29c0e17d63d0d7cfe0
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2026-04-07 10:04:50 -0300

    feat: implement CircuitBreakerService

---
+++ src/grpc/circuit-breaker.service.ts
@@ -0,0 +1,52 @@
+import { Injectable, Logger } from '@nestjs/common';
+import CircuitBreaker from 'opossum';
+import { GatewayService } from '@/gateway/gateway.service';
+
+type ServicesName = keyof ReturnType<GatewayService['serviceConfig']>;
+
+type BreakerAction = (fn: () => Promise<unknown>) => Promise<unknown>;
+
+@Injectable()
+export class CircuitBreakerService {
+  private readonly logger = new Logger(CircuitBreakerService.name);
+  private readonly breakers = new Map<
+    string,
+    CircuitBreaker<Parameters<BreakerAction>, unknown>
+  >();
+
+  constructor(private readonly gatewayService: GatewayService) {}
+
+  getBreaker(
+    serviceName: ServicesName
+  ): CircuitBreaker<Parameters<BreakerAction>, unknown> {
+    if (!this.breakers.has(serviceName)) {
+      const { timeout } = this.gatewayService.serviceConfig()[serviceName];
+
+      const breaker = new CircuitBreaker<Parameters<BreakerAction>, unknown>(
+        (fn: () => Promise<unknown>) => fn(),
+        {
+          name: serviceName,
+          timeout,
+          errorThresholdPercentage: 50,
+          resetTimeout: 30_000,
+          volumeThreshold: 5,
+        }
+      );
+
+      breaker.on('open', () =>
+        this.logger.warn(`[${serviceName}] Circuit breaker OPEN`)
+      );
+      breaker.on('halfOpen', () =>
+        this.logger.log(`[${serviceName}] Circuit breaker HALF-OPEN`)
+      );
+      breaker.on('close', () =>
+        this.logger.log(`[${serviceName}] Circuit breaker CLOSED`)
+      );
+
+      this.breakers.set(serviceName, breaker);
+    }
+
+    // biome-ignore lint/style/noNonNullAssertion: key was just set above
+    return this.breakers.get(serviceName)!;
+  }
+}
